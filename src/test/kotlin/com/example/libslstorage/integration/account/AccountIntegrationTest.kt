f7912f47319888c47ee84bf73ed04fa911806259
Михаил Лихолетов <odaving111@gmail.com>
2023-06-15 18:49:51 +0300

    Исправления тестов пользователей

--- src/test/kotlin/com/example/libslstorage/integration/account/AccountIntegrationTest.kt
+++ src/test/kotlin/com/example/libslstorage/integration/account/AccountIntegrationTest.kt
@@ -1,12 +1,18 @@
 package com.example.libslstorage.integration.account
 
+import com.example.libslstorage.dto.BLANK_NAME_VALIDATION_MESSAGE
+import com.example.libslstorage.dto.INVALID_EMAIL_VALIDATION_MESSAGE
+import com.example.libslstorage.dto.INVALID_PASSWORD_SIZE_VALIDATION_MESSAGE
 import com.example.libslstorage.dto.account.AccountResponse
 import com.example.libslstorage.dto.account.CreateAccountRequest
+import com.example.libslstorage.entity.AccountEntity
+import com.example.libslstorage.exception.EMAIL_ALREADY_EXISTS_ERROR_MESSAGE
 import com.example.libslstorage.integration.AbstractIntegrationTest
 import com.example.libslstorage.repository.AccountRepository
+import com.example.libslstorage.util.ACCESS_TOKEN_COOKIE_NAME
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
@@ -15,26 +21,27 @@ import org.springframework.security.crypto.password.PasswordEncoder
 class AccountIntegrationTest : AbstractIntegrationTest() {
 
     @Value("\${superuser.email}")
-    lateinit var superuserEmail: String
+    private lateinit var superUserEmail: String
 
     @Autowired
-    lateinit var passwordEncoder: PasswordEncoder
+    private lateinit var testUserAccount: AccountEntity
 
     @Autowired
-    lateinit var accountRepository: AccountRepository
+    private lateinit var passwordEncoder: PasswordEncoder
 
-    @BeforeEach
-    fun init() {
-        accountRepository.deleteAllByEmailNotLike(superuserEmail)
+    @Autowired
+    private lateinit var accountRepository: AccountRepository
+
+    @AfterEach
+    fun tearDown() {
+        transactionTemplate.execute {
+            accountRepository.deleteAllByEmailNotIn(setOf(testUserAccount.email, superUserEmail))
+        }
     }
 
     @Test
     fun `Should register account`() {
-        val request = CreateAccountRequest(
-            "test",
-            "test@test.example",
-            "password"
-        )
+        val request = CreateAccountRequest("new", "new@user.new", "password")
         webTestClient.post()
             .uri("/account/register")
             .bodyValue(request)
